aa4253b3b731e0414cbd5ab50ed34f12dec89e76
Mayankkkksss <mayankvishwakarma2955@gmail.com>
2026-06-18 13:58:11 +0530

    Final Version 1.0

---
+++ main.py
@@ -0,0 +1,33 @@
+import nltk
+nltk.download("punkt")
+nltk.download("punkt_tab")
+from nltk.tokenize import word_tokenize
+nltk.download("stopwords")
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+from flask import Flask, request, render_template
+
+
+
+app = Flask(__name__)
+
+@app.route('/', methods=["GET", "POST"])
+def homepage():
+    if request.method == "POST":
+        headline_text = request.form.get("headline_text")
+        stemmer = PorterStemmer()
+        token = word_tokenize(headline_text)
+
+        stop_words = set(stopwords.words("english"))
+        common_symbols = {',', '?', '.', '"', "'", ':', ';', '!', '@', '#', '$', '%', '*', '&', '(', ')'}
+        stop_words.update(common_symbols)
+        stop_words_list = [words for words in token if words.lower() not in stop_words]
+        stemmed_words = [stemmer.stem(words) for words in stop_words_list]
+        result = " ".join(stemmed_words)
+
+        return render_template("index.html", result = result)
+
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=False)
